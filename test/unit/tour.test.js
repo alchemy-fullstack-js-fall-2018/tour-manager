3216fb296e536535d86d414862afaf5f7289eb4a
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-10-11 08:52:37 -0700

    additional scaffold and validate model

--- test/unit/tour.test.js
+++ test/unit/tour.test.js
@@ -1,5 +1,5 @@
 const Tour = require('../../lib/models/tour');
-const { getErrors } = require('./helpers');
+const { getErrors } = require('../util/helpers');
 
 
 describe('tour model', () => {
@@ -30,6 +30,15 @@ describe('tour model', () => {
         expect(jsonTour).toEqual({ ...data, stops: expect.any(Array), _id: expect.any(Object) });
         expect(jsonTour.stops[0]).toEqual({ ...data.stops[0], _id: expect.any(Object) });
         expect(jsonTour.stops).toHaveLength(1);
+    });
+
+    it('confirms that title is required', () => {
+        const tour = new Tour({
+            activities:['one ride that is broken', 'smelly petting zoo'],
+            launchDate: Date.now()
+        });
+        const errors = getErrors(tour.validateSync(), 1);
+        expect(errors.title.properties.message).toEqual('Path `title` is required.');
     });
 
 });
