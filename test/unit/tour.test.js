1f3534049d984069c4c9540f370f7b2a2c4ebac3
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-10-10 17:23:28 -0700

    initial commit

---
+++ test/unit/tour.test.js
@@ -0,0 +1,11 @@
+require('dotnev').config();
+const Tour = require('../../lib/models/tour');
+require('../../lib/util/connect')();
+
+describe('tour model', () => {
+
+    it('creates a tour in my db', () => {
+        
+    });
+
+});
